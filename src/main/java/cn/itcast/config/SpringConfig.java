85b10ccc3a9b16a6d3777bb7f8da16e07ddcdaa6
lxy777 <314872695@qq.com>
2020-06-28 10:38:55 +0800

    SpringUtils多余配置删除

--- src/main/java/cn/itcast/config/SpringConfig.java
+++ src/main/java/cn/itcast/config/SpringConfig.java
@@ -2,12 +2,10 @@ package cn.itcast.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 // spring配置类
 @Configuration
 @ComponentScan(value = {"cn.itcast","cn.hutool.extra.spring"})
-@Import(cn.hutool.extra.spring.SpringUtil.class)
 public class SpringConfig {
 
 }
